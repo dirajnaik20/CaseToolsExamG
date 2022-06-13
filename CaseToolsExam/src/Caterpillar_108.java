ce4917b9b050feac167dbfb9d65d0b7c63584f02
admin <admin@Bcalab4-m09>
2022-06-13 09:08:31 +0530

    Commit1

---
+++ CaseToolsExam/src/Caterpillar_108.java
@@ -0,0 +1,19 @@
+//R/BCA-20-108
+
+
+public class Caterpillar_108 {
+	int moves,cabbages;
+	
+	Caterpillar_108(int moves,int cabbages)
+	{
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
